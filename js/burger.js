31422ea85501e22b3ae3ff4688f61948bcb58db8
Pavel Demidovich <dusbtr1@gmail.com>
2017-11-21 00:47:13 +0300

    burger

---
+++ js/burger.js
@@ -0,0 +1,32 @@
+let menu = (function(options) {
+    
+      let button = document.querySelector(options.button);
+      let menu = document.querySelector(options.menu);
+      let body = document.querySelector('body');
+    
+      let _toggleMenu = function(e) {
+        button.classList.toggle('header__burger-btn--active');
+        menu.classList.toggle('overlay--active');
+        body.classList.toggle('body--active');
+      }
+    
+      let addListeners = function() {
+        button.addEventListener('click', _toggleMenu);
+      }
+    
+      return {
+        init: addListeners
+      };
+    })({
+      button: '#toggle',
+      menu: '#overlay'
+    });
+    
+    menu.init();
+    
+    // $('#toggle').click(function() {
+    //   $(this).toggleClass('button_container--active');
+    //   $('#overlay').toggleClass('overlay--open');
+    //   $('body').toggleClass('body-active-menu');
+    // });
+    
