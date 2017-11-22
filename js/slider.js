09278ffcf4d039d0005175cc976e09110b037ff6
Pavel Demidovich <dusbtr1@gmail.com>
2017-11-22 22:54:57 +0300

    burger

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){
-  
-    $('.burger-wrapper').bxSlider({
-        speed : 1000
+$(document).ready(function () {
+    slider = $('.burger-wrapper').bxSlider({
+        speed : 600     
     });
-  });
+    $('.burger__arrow-right').on('click', function(e){
+        e.preventDefault();
+        slider.goToNextSlide();
+    });
+    $('.burger__arrow-left').on('click', function(e){
+        e.preventDefault();
+        slider.goToPrevSlide();
+    });
+});
