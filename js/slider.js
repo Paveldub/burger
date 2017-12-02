d2797be0abc6f0f6caff4aff8589fd1d5d39d084
Pavel Demidovich <dusbtr1@gmail.com>
2017-12-02 18:25:21 +0300

    burger

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     slider = $('.burger-wrapper').bxSlider({
-        speed : 600     
+        speed : 600, 
+        slideMargin: 41
     });
     $('.burger__arrow-right').on('click', function(e){
         e.preventDefault();
